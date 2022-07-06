76e7e217ccc3cee738e80d8fa90a1baa4b0be44b
manhthang150801 <91358021+manhthang150801@users.noreply.github.com>
2022-07-06 23:48:18 +0700

    Create sochiahet.sh

---
+++ Tuan5/bai1/sochiahet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+function sochiahet(){
+	echo -e -n "\n\t=>Cac so chia het cho 7 la: "
+	for(( i=1; i<=$1; i++ ))
+	do
+		chia=`expr $i % 7`
+		if [ $chia -eq 0 ]
+			then 
+				echo -n "$i, "
+		fi
+	done
+}
+n=$1
+sochiahet $n
