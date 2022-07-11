f81a485db55240fb31aa35cc3f5ab06eefb5c214
manhthang150801 <91358021+manhthang150801@users.noreply.github.com>
2022-07-11 17:02:59 +0700

    Update renamefile.sh

--- Tuan5/bai5/renamefile.sh
+++ Tuan5/bai5/renamefile.sh
@@ -1,7 +1,24 @@
-echo "Nhap ten dau file goc:"
-read manh
-echo "Ten moi:"
-read thang
-# Đổi tên tất cả các tệp phù hợp với mẫu
-mv -- "$manh.txt" "$thang.txt"
-echo "Các tệp đã được đổi tên."
+#!/bin/sh
+num=0
+while [ $num -eq 0 ]
+	do 
+		echo "Nhap ten file goc:"
+		read manh
+		if test -e "$manh"
+			then
+				echo "file da ton tai"
+				echo "Nhap ten dau file moi:"
+				read thang
+					if test -f "$manh"
+						then 
+							mv -- "$manh" "$thang.txt"
+							echo "Doi thanh cong"
+							exit 0
+					else
+						echo "Khong thanh cong"
+					fi
+		else
+			echo "file khong ton tai"
+		fi
+	done
+
