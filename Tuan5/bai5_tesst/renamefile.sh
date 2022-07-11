0d41796766c0352ca716a1489445e1cadb95e47c
manhthang150801 <91358021+manhthang150801@users.noreply.github.com>
2022-07-11 20:07:40 +0700

    Create renamefile.sh

---
+++ Tuan5/bai5_tesst/renamefile.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+num=0
+while [ $num -eq 0 ]
+	do 
+		echo "Nhap ten file goc:"
+		read manh
+		if test -e "$manh"
+			then
+				echo "file da ton tai"
+				echo "Nhap ten dau file moi:"
+				read thang
+					if test -f "$manh"
+						then 
+							mv -- "$manh" "$thang.txt"
+							echo "Doi thanh cong"
+							ls -l *.txt
+					else
+						echo "Khong thanh cong"
+					fi
+		else
+			echo "file khong ton tai"
+		fi
+	done
+
